ef91757f53d6b2706a604fe8c47ad90849d248bd
SalavatKhR <SalavatKhR@gmail.com>
2021-11-02 12:50:56 +0300

    Initial

---
+++ hw7/hw7/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using hw7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace hw7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] UserProfile profile)
+        {
+            return View(profile);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new UserProfile("Daniil", "Dedashev", UserProfile.Gender.Male, 19));
+        }
+        
+        [HttpGet]
+        public IActionResult MyMethod()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ hw7/hw7/Models/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hw7.Models
+{
+    public record UserProfile(
+        [Required]string FirstName, [Required]string LastName, UserProfile.Gender? gender, int? age)
+    {
+        //public string? FirstName { get; set; }
+        //public string? LastName { get; set; }
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+        
+    }
+}
