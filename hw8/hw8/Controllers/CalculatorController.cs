8055a5d89693bbf77a618ca25d75144859e5e04f
SalavatKhR <SalavatKhR@gmail.com>
2021-11-23 20:21:27 +0300

    Services added

---
+++ hw8/hw8/Controllers/CalculatorController.cs
@@ -0,0 +1,14 @@
+﻿using hw8.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hw8.Controllers
+{
+    public class CalculatorController: Controller
+    {
+        [HttpGet, Route("calculate")]
+        public IActionResult Calculate([FromServices] ICalculator calculator, string arg1, string op, string arg2)
+        {
+            return Content(calculator.Calculate(arg1, op, arg2));
+        }
+    }
+}
---
+++ hw8/hw8/Services/Calculator.cs
@@ -0,0 +1,32 @@
+﻿namespace hw8.Services
+{
+    public class Calculator: ICalculator
+    {
+        public bool TryParseArgs(string arg1, string arg2, out double val1, out double val2)
+        {
+            var isVal1double = double.TryParse(arg1, out val1);
+            var isVal2double = double.TryParse(arg2, out val2);
+            return isVal1double && isVal2double;
+        }
+        public string Calculate(string arg1, string op, string arg2)
+        {
+            var isValidArgs = TryParseArgs(arg1, arg2, out var val1, out var val2);
+            string result;
+            if(isValidArgs)
+                result = op switch
+                {
+                    "plus" => $"{val1 + val2}",
+                    "minus" => $"{val1 - val2}",
+                    "multiply" => $"{val1 * val2}",
+                    "divide" => (val2 != 0) ? $"{val1 / val2}" : "Divide by zero!",
+                    _ => "Wrong operation!"
+                };
+            else
+            {
+                result = "Wrong arguments!";
+            }
+            
+            return result ;
+        }
+    }
+}
