e37f4aae2fe3e4df4500fae4031d70499519210a
SalavatKhR <SalavatKhR@gmail.com>
2021-12-06 17:36:11 +0300

    CachingCalculator done(needs refactoring)

---
+++ hw10/hw10/Models/Cache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace hw10.Models
+{
+    public class Cache
+    {
+        [Key]
+        public string Expression { get; set; }
+        public string Value { get; set; }
+
+        public Cache(string expression, string value)
+        {
+            Expression = expression;
+            Value = value;
+        }
+    }
+}
