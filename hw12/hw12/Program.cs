704a357c153197e28effb305f384573dba98d722
SalavatKhR <SalavatKhR@gmail.com>
2021-12-14 23:50:32 +0300

    Benchmarks added

---
+++ hw12/hw12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+
+namespace hw12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MyBenchmarks>();
+        }
+    }
+}
