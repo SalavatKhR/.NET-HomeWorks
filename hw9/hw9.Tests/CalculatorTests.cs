89942080b67e69ba4a4ae0b453a3f8638258c5ab
SalavatKhR <SalavatKhR@gmail.com>
2021-12-18 12:17:45 +0300

    operations changed

--- hw9/hw9.Tests/CalculatorTests.cs
+++ hw9/hw9.Tests/CalculatorTests.cs
@@ -26,11 +26,11 @@ namespace hw9.Tests
         }
         
         [Theory]
-        [InlineData("1plus5plus3","9")]
-        [InlineData("5plus3minus2","6")]
-        [InlineData("5plus1plus2multiply3","12")]
-        [InlineData("5plus%281plus2%29multiply3","14")]
-        [InlineData("6divide3divide2","1")]
+        [InlineData("1%2b5%2b3","9")]
+        [InlineData("5%2b3-2","6")]
+        [InlineData("5%2b1%2b2*3","12")]
+        [InlineData("5%2b%281%2b2%29*3","14")]
+        [InlineData("6/3/2","1")]
         [InlineData("3","3")]
         public async Task Calculate_withCorrectValues_ReturnsCorrect(string expression, string expected)
         {
