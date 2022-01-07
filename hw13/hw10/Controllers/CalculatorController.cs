89208147987a538482dbfd7463d8f3997e6f706c
SalavatKhR <SalavatKhR@gmail.com>
2022-01-07 11:13:49 +0300

    Db cache changed to memory cache

---
+++ hw13/hw10/Controllers/CalculatorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using hw10.Models;
+using hw10.Calculator;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace hw10.Controllers
+{
+    public class CalculatorController: Controller
+    {
+        private readonly ICachingCalculator _calculator;
+
+
+        public CalculatorController([FromServices]ICachingCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+        
+        
+        [HttpGet, Route("calculate")]
+        public IActionResult Calc(string input)
+        {
+            var result =  _calculator.CalculateWithCache(input);
+            return Content(result);
+        }
+    }
+}
---
+++ hw13/hw10/ExpressionTree/Visitor.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace hw10.ExpressionTree
+{
+    public class Visitor: ExpressionVisitor
+    {
+        
+    }
+}
